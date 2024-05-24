977232b35f03095ca50f284eac32a8f0c16fd0eb
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-05-24 14:14:36 +0200

    POST, GET and DELETE

--- server.js
+++ server.js
@@ -9,22 +9,33 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 const data =[
-    {name:"hans", age: 17},
-    {name:"paul", age: 17},
-    {name:"peter", age: 17},
-    {name:"sepp", age: 17},
+    {name:"hans", age: 17,id:0},
+    {name:"paul", age: 17,id:1},
+    {name:"peter", age: 17 ,id:2},
+    {name:"sepp", age: 17,id:3}
 ]
 
 
-app.get("/people", (req, res)=>
-{
+app.get("/people", (req, res)=>{
     res.send(data);
 });
 
-app.post("/people",(req,res)=>{
-    console.log(req.body);
-    res.send("thanks");
+app.get("/people/:id", (req, res)=>{
+    let id = req.params.id;
+    res.send(data[id]);
 });
+
+app.post("/people",(req,res)=>{
+    data.push(req.body);
+
+    res.send(req.body);
+});
+
+app.delete("/people/:id", (req,res)=>{
+    let id = req.params.id;
+    data.splice(id, 1);
+    res.send("done")
+})
 
 app.listen(port, ()=>{
     console.log("Server is running on port: " + port);
