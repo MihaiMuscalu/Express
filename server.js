67f6a1cf964ce5a13a9319dfff07132d4f75d8d9
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-05-27 08:46:33 +0200

    DELETE

--- server.js
+++ server.js
@@ -1,55 +1,56 @@
-
-const express = require("express")
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const db = require("./db");
 
-const app = express()
+const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const data =[
-    {name:"hans", age: 17,id:0},
-    {name:"paul", age: 17,id:1},
-    {name:"peter", age: 17 ,id:2},
-    {name:"sepp", age: 17,id:3}
-]
+const data = [
+  { name: "hans", age: 17, id: 0 },
+  { name: "paul", age: 17, id: 1 },
+  { name: "peter", age: 17, id: 2 },
+  { name: "sepp", age: 17, id: 3 },
+];
 
-
-app.get("/people", async (req, res)=>{
-    try {
-        let result = await db.query("SELECT * from people")
-        res.send(result);
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
-
+app.get("/people", async (req, res) => {
+  try {
+    let result = await db.query("SELECT * from people");
+    res.send(result);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
-
-app.post("/people",async (req,res)=>{
-    let person = req.body;
-    let sql = "insert into people values(?,?,?)";
-    let id = "SELECT MAX(ID) FROM people"
-    try {
-        let result = await db.query(sql, [parseInt(id)+1,person.firstname, person.lastname])
-        res.send(result);
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
+app.post("/people", async (req, res) => {
+  let person = req.body;
+  let sql = "insert into people values(?,?,?)";
+  let id = "SELECT MAX(ID) FROM people";
+  try {
+    let result = await db.query(sql, [
+      parseInt(id) + 1,
+      person.firstname,
+      person.lastname,
+    ]);
+    res.send(result);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
-app.delete("/people:PN",async (req,res)=>{
-    try {
+app.delete("/people/:PN", async (req, res) => {
+  console.log(req.params.PN);
+  try {
     let id = "delete from people where ID=?";
-    let result = await db.query(id,[req.params.PN])
-    res.send(result)
-    }catch (error) {
-        res.status(404).send(error.message);
-    }
-})
+    let result = await db.query(id, [req.params.PN]);
+    res.send(result);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
 
-app.listen(port, ()=>{
-    console.log("Server is running on port: " + port);
+app.listen(port, () => {
+  console.log("Server is running on port: " + port);
 });
